8eac5b242da0c80acb578ca34b363f6a65aa6649
fadimata tillman <fadi.tillman@gmail.com>
2019-12-30 03:53:32 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+def square_array(1,2,3,4)
+  counter = 0
+ 
+  while square_array[counter] do
+    puts square_array ^ [counter]
+    counter += 1
+  end
 end
